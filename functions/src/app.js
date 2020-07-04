219b898af0c8e9833d265459289a9678385731b3
ovezovs <shageldi.ovezov@gmail.com>
2020-07-03 22:15:48 -0400

    applied styles

--- functions/src/app.js
+++ functions/src/app.js
@@ -14,14 +14,19 @@ form.addEventListener('submit', event => {
         body: JSON.stringify({ name: form.elements.name.value })
     }).then(res => res.json()).then(res => {
         const session = OT.initSession(res.apiKey, res.sessionId)  // initialize the session on the client
-        const publisher = OT.initPublisher(document.getElementById("publishers"))           // create publisher, by default it is the camera
+        const publisher = OT.initPublisher("publishers")           // create publisher, by default it is the camera
         //connect to the session and then immediately publish the publisher
         session.connect(res.token, () => {
             session.publish(publisher)
         })
         // when someone else joins, subscribe to the stream
         session.on('streamCreated', event => {
-            session.subscribe(event.stream)
+            session.subscribe(event.stream, "subscribers", {
+                insertMode: 'append',
+                buttonDisplayMode: 'on',
+                audioLevelDisplayMode: 'on',
+                videoDisabledDisplayMode: 'on'
+            })
         })
     })
 })
